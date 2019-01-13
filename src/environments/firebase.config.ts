08cd14d837784479d6565fe2e468b8e134e3151d
hendr_8nkbeie <hendrik.herdler@stud.hszg.de>
2019-01-13 18:27:43 +0100

    Create and import firebase.config.ts.

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDgwL2EeO4pki1HBudoOGH6ubbonRYPD64',
+    authDomain: 'hh-firebase-proj001.firebaseapp.com',
+    databaseURL: 'https://hh-firebase-proj001.firebaseio.com',
+    // projectId: "hh-firebase-proj001",
+    storageBucket: 'hh-firebase-proj001.appspot.com'
+    // messagingSenderId: "312713005144"
+
+};
